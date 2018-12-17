56bb6c69157b14c34a3e29f96d50952a4bf6ffc5
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-17 14:41:44 -0500

    adding StreamForm reusable comp, using patch over put for editStream, etc

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { 
    SIGN_IN,
    SIGN_OUT,
@@ -30,6 +31,8 @@ export const createStream = (formValues) => async (dispatch, getState) => {
    const response = await streams.post('/streams', { ...formValues, userId })
 
    dispatch({ type: CREATE_STREAM, payload: response.data })
+   // Do some programmatic navigation to get user back to stream List page
+   history.push('/')
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -45,9 +48,10 @@ export const fetchStream = (id) => async dispatch => {
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-   const response = await streams.put(`/streams/${id}`, formValues);
+   const response = await streams.patch(`/streams/${id}`, formValues);
 
    dispatch({ type: EDIT_STREAM, payload: response.data })
+   history.push('/')
 };
 
 export const deleteStream = (id) => async dispatch => {
